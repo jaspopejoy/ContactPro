f09fc042cf9fc90def0698db38332f59b271baee
jaspopejoy <120056960+jaspopejoy@users.noreply.github.com>
2023-06-07 12:20:07 -0500

    final commit

--- Program.cs
+++ Program.cs
@@ -12,8 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
-//var connectionString = ConnectionHelper.GetConnectionString(builder.Configuration);
+//var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
+var connectionString = ConnectionHelper.GetConnectionString(builder.Configuration);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
